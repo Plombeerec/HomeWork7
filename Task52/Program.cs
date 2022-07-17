2dd6c14eaa3144deebe002769bfd5ac7335f7193
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-07-17 10:15:42 +0300

    Task 52 выполняется только для квадратных массивов

---
+++ Task52/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+
+
+int[,] CreateRandomArray(int rows, int columns, int min, int max)
+{
+    int[,] newArray = new int[rows, columns];
+
+    for(int i = 0; i <rows; i++)
+    {
+        for(int j = 0; j <columns; j++)
+        {
+            newArray[i,j] = new Random().Next(min, max+1);
+        }
+    }
+    return newArray;
+}
+
+void ShowArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++ )
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void Average(int[,] array)
+{
+    int count = 0;
+    double result = 0;
+    for(int i = 0; i < array.GetLength(0); i++ )
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if( j == 0)
+            {
+                count = 0;
+            }
+            count +=array[j,i];
+            result = (double)count / (array.GetLength(1)) ;
+        }
+        Console.WriteLine("Count is "+  count);
+        Console.WriteLine("Result is " + result);
+    }
+    return ;
+}
+
+
+Console.WriteLine("input number of rows");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input number of columns");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandomArray(m , n, 1, 9);
+ShowArray(myArray);
+Average(myArray);
